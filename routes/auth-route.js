9deb8f3c4182944a253ca1e9f035eff699fdc33d
phaewphanwrd <phaewphanwrd@gmail.com>
2025-03-12 13:06:42 +0700

    Update

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -5,4 +5,4 @@ const authController = require("../controllers/auth-controller")
 // @ENDPOINT http://localhost:8877/api/auth/...
 router.post("/auth/register", authController.register)
 
-module.exports = router
+module.exports = router 
---
+++ routes/booking-route.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const router = express.Router()
+
+router.post("/booking/create")
+
+module.exports = router
