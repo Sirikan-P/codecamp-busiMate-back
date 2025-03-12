8bef71dcc2b3144591e19643c83dd2ab14a41079
BuayCh <buaykemm14@gmail.com>
2025-03-12 15:59:46 +0700

    update client-user

---
+++ controllers/user-controller/user-controller.js
@@ -0,0 +1,55 @@
+const prisma = require("../../configs/prisma")
+
+exports.showUser = async (req, res, next) => {
+
+    try {
+        const { email } = req.user
+        const users = await prisma.user.findFirst({
+            select: {
+                id: true,
+                email: true,
+                firstName: true,
+                lastName: true,
+                phoneNumber: true,
+            }
+        })
+        console.log(users)
+        res.json({ msg: "Show user" })
+    } catch (error) {
+        next(error)
+    }
+}
+exports.editUser = (req, res, next) => {
+    try {
+        res.json({ msg: "Update user" })
+    } catch (error) {
+        next(error)
+    }
+}
+exports.addPatients = (req, res, next) => {
+    try {
+        res.json({ msg: "Add patients" })
+    } catch (error) {
+        next(error)
+    }
+}
+exports.editPatients = (req, res, next) => {
+    try {
+        res.json({ msg: "Update patients" })
+    } catch (error) {
+        next(error)
+    }
+}
+exports.deletePatients = async(req, res, next) => {
+    try {
+        const { id } = req.params
+        const deleted = await prisma.patient.delete({
+            where:{
+                id:Number(id)
+            }
+        })
+        res.json({ msg: "Delete patients" })
+    } catch (error) {
+        next(error)
+    }
+}
--- routes/user-route.js
+++ routes/user-route.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const userController = require('../controllers/user-controller')
+const userController = require('../controllers/user-controller/user-controller')
 
-router.get('/api/user/me/:id',userController.showUser)
-router.patch('/api/user/me/:id',userController.editUser)
-router.post('/api/user/patient',userController.addPatients)
-router.patch('/api/user/patient/:id',userController.editPatients)
+router.get('/api/user/me',userController.showUser)
+router.patch('/api/user/me/edit',userController.editUser)
+router.post('/api/user/patient/add',userController.addPatients)
+router.patch('/api/user/patient/edit',userController.editPatients)
 router.delete('/api/user/patient/:id',userController.deletePatients)
 
 module.exports = router
