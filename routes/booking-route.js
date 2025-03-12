05e66a6401a24942ed8c92727b0f9665af0c3f31
phaewphanwrd <phaewphanwrd@gmail.com>
2025-03-12 17:18:01 +0700

    create seed.js resetDB.js/ create booking-controller.js

--- routes/booking-route.js
+++
@@ -1,6 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-router.post("/booking/create")
-
-module.exports = router
--- routes/user-route.js
+++ routes/user-route.js
@@ -1,8 +1,16 @@
 const express = require("express")
 const router = express.Router()
+const userController = require("../controllers/user-controller/booking-controller")
+
+
+router.post("/booking/create", userController.createBooking)
+router.get("/booking/get", userController.getBooking)
+router.patch("/booking/cancel", userController.cancelBooking)
 
 
 // @ENDPOINT http://localhost:8877/api/user/...
 //
 
 module.exports = router
+
+
