f0b23429d2e196339bb0bc77f0acedd51467ce25
chatchai <chsuriyawan@yahoo.com>
2025-03-18 15:31:28 +0700

    add route /user/findDriverByName and its controller

--- routes/admin-route.js
+++ routes/admin-route.js
@@ -2,7 +2,9 @@ const express = require("express")
 const router = express.Router()
 const adminDashboardController = require("../controllers/admin/admin-dashboard-controller")
 const adminUserController = require("../controllers/admin/admin-user-controller")
+const adminReportController = require("../controllers/admin/admin-report-controller")
 const { adminAuth } = require("../middlewares/auth-admin")
+const { authCheck } = require("../middlewares/authCheck")
  
 
 // @ENDPOINT http://localhost:8877/api/admin/...
@@ -12,7 +14,12 @@ router.get("/user/getUserDataAll", adminAuth, adminUserController.getUserDataAll
 router.patch("/user/updateUserData/:id", adminAuth, adminUserController.updateUserData)
 router.patch("/user/deleteUser/:id", adminAuth, adminUserController.deleteUser)
 router.get("/user/getDriverDataAll", adminAuth, adminUserController.getDriverDataAll)
+router.get("/user/findDriverByName", adminAuth, adminUserController.findDriverByName)
 router.patch("/user/updateDriverData/:id", adminAuth, adminUserController.updateDriverData)
 router.patch("/user/deleteDriver/:id", adminAuth, adminUserController.deleteDriver)
+router.post("/report/createFeedbackReport", adminAuth, adminReportController.createFeedbackReport)
+router.get("/report/getAllFeedbackReport", adminAuth, adminReportController.getAllFeedbackReport)
+router.get("/report/getFeedbackReportById/:id", adminAuth, adminReportController.getFeedbackReportById)
+router.patch("/report/updateFeedbackReportById/:id", adminAuth, adminReportController.updateFeedbackReportById)
 
 module.exports = router
