a5341a326975377b1f849948612ec8daa8a1585f
Sirikan-P <sirikan.interior@gmail.com>
2025-03-21 02:08:35 +0700

    notifycation 2way

--- utils/notiService.js
+++ utils/notiService.js
@@ -1,12 +1,22 @@
 
+
 const notiService = (io) => {
-  return  io.on('connection', (socket) => {
+  //console.log('socket')
+  return io.on('connection', (socket) => {
     console.log('a user connected');
     //create socket for noti 
-    socket.on('send_noti',(driver)=>{
-      //console.log(driver)
-      io.emit(driver.id, driver.id)   // ("event name" , "data" )
-    })   
+    socket.on('send_noti', (booking) => {
+      console.log(booking.driverId)      
+      io.emit("U"+booking.driverId, booking )   // ("event name" , "data" )
+    })
+
+    socket.on('driver_noti', (data) => {
+      console.log(data)
+      //if reject = set database booking status = FIND_DRIVER
+      //if accept = set database booking status = UP_COMING
+      io.emit(data.bookingId, data.result)   // ("event name" , "data" )
+    })
+
   });
 }
 
