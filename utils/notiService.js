532228b22e62c55422c365fcf0685ae2a93287a9
Sirikan-P <sirikan.interior@gmail.com>
2025-03-21 17:21:33 +0700

    booking status

--- utils/notiService.js
+++ utils/notiService.js
@@ -1,4 +1,4 @@
-
+const prisma = require("../configs/prisma")
 
 const notiService = (io) => {
   //console.log('socket')
@@ -10,10 +10,20 @@ const notiService = (io) => {
       io.emit("U"+booking.driverId, booking )   // ("event name" , "data" )
     })
 
-    socket.on('driver_noti', (data) => {
+    socket.on('driver_noti',async  (data) => {
       console.log(data)
       //if reject = set database booking status = FIND_DRIVER
       //if accept = set database booking status = UP_COMING
+      const bookingStatus =  (data.result=='ACCEPT') ? "UP_COMING": "FIND_DRIVER"
+          // อัปเดตสถานะ 
+       await prisma.booking.update({
+        where: { id: +data.bookingId },
+        data: {
+          bookingStatus : bookingStatus ,
+          driverId : +data.driverId ,          
+        }
+      });
+
       io.emit(data.bookingId, data.result)   // ("event name" , "data" )
     })
 
