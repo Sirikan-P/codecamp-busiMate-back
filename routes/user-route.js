f669cf20ee4879d61261762d2d18ce0a92079dda
Sirikan-P <sirikan.interior@gmail.com>
2025-03-21 17:03:44 +0700

    payment

--- routes/user-route.js
+++ routes/user-route.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require('../controllers/user-controller/user-controller')  
 const bookingController = require("../controllers/user-controller/booking-controller");
+const payment = require("../controllers/payment-controller")
 
 const { authCheck } = require("../middlewares/authCheck");
 
@@ -18,5 +19,13 @@ router.patch('/patient/edit',authCheck,userController.editPatients)
 
 // @ENDPOINT http://localhost:8877/api/user/...
 //
+
+
+
+
+
+//@ENDPOINT http://localhost:8877/api/user/payment/checkout
+router.post('/payment/checkout' ,payment.checkOut )  
+router.get('/payment/checkout-status/:session_id',payment.checkOutStatus )  
 
 module.exports = router;
