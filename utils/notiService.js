83791477e28049f4dac1b6496b39ffccfba7604c
Sirikan-P <sirikan.interior@gmail.com>
2025-03-21 17:56:34 +0700

    close log

--- utils/notiService.js
+++ utils/notiService.js
@@ -3,15 +3,15 @@ const prisma = require("../configs/prisma")
 const notiService = (io) => {
   //console.log('socket')
   return io.on('connection', (socket) => {
-    console.log('a user connected');
+    //console.log('a user connected');
     //create socket for noti 
     socket.on('send_noti', (booking) => {
-      console.log(booking.driverId)      
+      //console.log(booking.driverId)      
       io.emit("U"+booking.driverId, booking )   // ("event name" , "data" )
     })
 
     socket.on('driver_noti',async  (data) => {
-      console.log(data)
+      //console.log(data)
       //if reject = set database booking status = FIND_DRIVER
       //if accept = set database booking status = UP_COMING
       const bookingStatus =  (data.result=='ACCEPT') ? "UP_COMING": "FIND_DRIVER"
