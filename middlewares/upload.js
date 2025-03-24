878d8b656adee07e10e18608dfc666faf46ab55c
edenpuma33 <edenpuma33@gmail.com>
2025-03-24 12:23:09 +0700

    merge feature

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,15 +1,13 @@
 const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadDir = "uploads/";
-    if (!require("fs").existsSync(uploadDir)) {
-      require("fs").mkdirSync(uploadDir);
-    }
-    cb(null, uploadDir);
+  destination: (req, file, cb) =>
+    cb(null, path.join(__dirname, "../upload-pic")),
+  filename: (req, file, cb) => {
+    let fileExt = path.extname(file.originalname);
+    cb(null, `pic_${Date.now()}_${Math.round(Math.random() * 100)}${fileExt}`);
   },
-  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
 });
 
-const upload = multer({ storage });
-module.exports = upload;
+module.exports = multer({ storage: storage });
