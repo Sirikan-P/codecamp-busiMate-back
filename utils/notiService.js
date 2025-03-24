530b65ac526b6d15487233d23180458b800ca255
Sirikan-P <sirikan.interior@gmail.com>
2025-03-25 01:28:00 +0700

    fix noti error

--- utils/notiService.js
+++ utils/notiService.js
@@ -4,29 +4,28 @@ const notiService = (io) => {
   //console.log('socket')
   return io.on('connection', (socket) => {
     //console.log('a user connected');
-    //create socket for noti 
+    //create socket for noti -------------------
     socket.on('send_noti', (booking) => {
       //console.log("hello noti" ,booking)      
-      io.emit("U"+booking.driverId, booking )   // ("event name" , "data" )
+      io.emit("U"+booking.driverId, booking )   // ("event name: driver ID" , "data" )
     })
 
-    socket.on('driver_noti',async  (data) => {
-      
-      //if reject = set database booking status = FIND_DRIVER
-      //if accept = set database booking status = UP_COMING
+    socket.on('driver_noti',async  (data) => {      
+      //if reject = set database booking status = FIND_DRIVER---------------
+      //if accept = set database booking status = UP_COMING----------------
       const bookingStatus =  (data.result=='ACCEPT') ? "UP_COMING": "FIND_DRIVER"
-      console.log("hello noti from driver to user",bookingStatus)
+      //console.log("hello noti from driver to user",data)
      
-      //อัปเดตสถานะ 
+      // //อัปเดตสถานะ ---------------
        await prisma.booking.update({
-        where: { id: +data.bookingId },
+        where: { id: +data.id },
         data: {
           bookingStatus : bookingStatus ,
           driverId : +data.driverId ,          
         }
       });
 
-      io.emit(data.bookingId, data.result)   // ("event name" , "data" )
+      io.emit(data.id, data.result)   // ("event name:Booking Id" , "data" )
     })
 
   });
